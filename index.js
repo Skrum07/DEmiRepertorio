ca2be2fe04865a3d4065661230b4d127dc53dcdb
Tomas Roa <tomas.roa@gmail.com>
2024-07-10 00:51:55 -0400

    se crean archivos y rutas para el desarrollo del proyecto

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import router from "./router/router.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+//Midelwares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//routes
+app.use("/", router); 
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:PORT`));
